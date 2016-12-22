5c3ecffe6e4aeb980f232a8c00f7476237be86bd
Muaz <redkowloon@gmail.com>
2016-12-23 02:26:47 +0800

    update UserController
    Signed-off-by: Muaz <redkowloon@gmail.com>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UserController extends Controller
+class UserController extends BaseController
 {
-    //
+    public function index()
+    {
+        return view('user',compact('id',$this->getUserId()));
+    }
+
 }
