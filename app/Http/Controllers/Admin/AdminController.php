bf8c4798e27d0077a59af36038936c6ace4a48c0
Muaz <redkowloon@gmail.com>
2016-12-24 02:57:58 +0800

    update Controllers/Admin/AdminController.php
    Signed-off-by: Muaz <redkowloon@gmail.com>

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\BaseController;
+use Illuminate\Http\Request;
+
+class AdminController extends BaseController
+{
+
+    public function index()
+    {
+        return view('admin.index');
+    }
+}
